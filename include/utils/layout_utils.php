86c80365afc3d31e1722ac160f97c881a27933f2
ibetanzos <ibetanzos@nextel.es>
2014-01-14 17:16:51 +0100

    Fix line 342 to write correct meta http-equiv="Content-Type" content
    Now the charset var is not correctly set (cause the var is not being expanded) in meta http-equiv="Content-Type" content=
    Now: <meta http-equiv="Content-Type" content="text/html; charset=" {$charset}">
    Fixed so it now takes the var valor 
    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">

--- include/utils/layout_utils.php
+++ include/utils/layout_utils.php
@@ -339,7 +339,7 @@ EOHTML;
     if ($includeJS)
     {
         $charset = isset($app_strings['LBL_CHARSET']) ? $app_strings['LBL_CHARSET'] : $sugar_config['default_charset'];
-        echo '<meta http-equiv="Content-Type" content="text/html; charset="{$charset}">';
+        echo '<meta http-equiv="Content-Type" content="text/html; charset='."{$charset}".'">';
         echo '<script type="text/javascript" src="' . getJSPath('cache/include/javascript/sugar_grp1_yui.js') . '"></script>';
         echo '<script type="text/javascript" src="' . getJSPath('cache/include/javascript/sugar_grp1.js') . '"></script>';
         echo '</head>';
